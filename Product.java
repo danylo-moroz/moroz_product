488073f06ae8435a72c206aade22bda5cb712e4e
vaplite@gmail.com <dan21628@spengergasse.at>
2021-06-20 18:25:43 +0200

    Moroz: create name

---
+++ Product.java
@@ -0,0 +1,19 @@
+public ckass Product
+{
+ private String name;
+
+ public Product(String name)
+ {
+  setName(name);
+ }
+
+ public String name()
+ {
+   return name;
+ }
+
+ public void setName(String name)
+ {
+   this.name = name;
+ }
+}
